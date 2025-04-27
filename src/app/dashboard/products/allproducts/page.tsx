fad5165a9ea70c67227f33647753a2749f145742
benjy0011 <152722201+benjy0011@users.noreply.github.com>
2025-04-27 15:18:43 +0800

    Route exercise

---
+++ src/app/dashboard/products/allproducts/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AllProducts = () => {
+  return (
+    <div>(1220) products in the store</div>
+  )
+}
+
+export default AllProducts
---
+++ src/app/dashboard/products/specificproduct/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SpecificProduct = () => {
+  return (
+    <div>Here is my one & only product</div>
+  )
+}
+
+export default SpecificProduct
